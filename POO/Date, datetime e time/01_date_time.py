9f0b5183c66047cba955f85c694f167d9f18a1cd
Rodrigo Charles <rcharlescolors@gmail.com>
2024-05-27 12:54:42 -0300

    datetime, timedelta e desafio

---
+++ POO/Date, datetime e time/01_date_time.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime, time
+
+print(date(2024, 5, 24))
+
+print(date.today())
+
+data_hora = datetime(2024, 5, 24, 10, 11, 15)
+print(data_hora)
+print(datetime.today())
+
+print(time(10, 14))
